1f6844a77386f7730269f81a7f7ea77c520c985b
Adedoyin <isradeoluwa@gmail.com>
2022-03-31 14:25:18 +0100

    Custom Card: Listview builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_store_ui/themes/colors.dart';
 import 'package:e_commerce_store_ui/widgets/appbar.dart';
+import 'package:e_commerce_store_ui/widgets/card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -31,31 +32,56 @@ class HomeScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: List.generate(
-              categories.length,
+              _categories.length,
               (index) => Chip(
-                label: Text(categories[index]),
+                label: Text(_categories[index]),
                 backgroundColor:
                     index != 0 ? Colorz.ghostWhite : Colorz.pinkColor,
               ),
             ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 180,
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _imageUrls.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, index) => CustomCard(
+                      imageUrl: _imageUrls[index],
+                      itemName: _itemName[index],
+                      itemPrice: _itemPrice[index],
+                    )),
+          )
         ],
       ),
     );
   }
 
-  final List<String> categories = const [
+  final List<String> _categories = const [
     'Coats',
     'Dresses',
     'Jersey',
     'Pants',
   ];
 
-  final List<String> imageUrls = [
-    'https://images.asos-media.com/products/asos-design-double-breasted-coat-in-black/23385810-1-black?\$n_240w\$&wid=40&fit=constrain',
+  final List<String> _imageUrls = [
+    'https://images.asos-media.com/products/asos-design-double-breasted-coat-in-black/23385810-1-black?\$n_320w\$&wid=317&fit=constrain',
     'https://images.asos-media.com/products/forever-new-smart-collar-pea-coat-in-black/200414986-1-black?\$n_320w\$&wid=317&fit=constrain',
     'https://images.asos-media.com/products/mango-tailored-pea-coat-in-light-blue/201685473-1-blue?\$n_320w\$&wid=317&fit=constrain',
     'https://images.asos-media.com/products/forever-new-curve-top-faux-fur-collar-coat-in-grey/200987180-1-grey?\$n_320w\$&wid=317&fit=constrain',
     'https://images.asos-media.com/products/forever-new-petite-smart-collar-pea-coat-in-grey/200460866-1-cream?\$n_320w\$&wid=317&fit=constrain'
   ];
+
+  final List<String> _itemName = [
+    'Double Breasted Coat',
+    'Smart Collar Pea Coat',
+    'Tailored Pea Coat',
+    'Curve Top Faux Collar Coat',
+    'Petite Smart Collar Pea Coat'
+  ];
+
+  final List<String> _itemPrice = ['50', '43', '47', '47', '49'];
 }
--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -2,14 +2,29 @@ import 'package:e_commerce_store_ui/themes/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({Key? key}) : super(key: key);
+  final String? imageUrl;
+  final String? itemName;
+  final String? itemPrice;
+  const CustomCard({Key? key, this.imageUrl, this.itemName, this.itemPrice})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colorz.white,
-      child: Column(children: [
-        Image.network('')
-      ],),);
+      child: Column(
+        children: [
+          Expanded(flex: 8, child: Image.network(imageUrl!)),
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [Text(itemName!), Text('\$$itemPrice')],
+            )),
+          )
+        ],
+      ),
+    );
   }
 }
