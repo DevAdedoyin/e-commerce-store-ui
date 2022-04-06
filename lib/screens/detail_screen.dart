a6e44f98964c75a3690a15f1c08dba21d40ca372
Adedoyin <isradeoluwa@gmail.com>
2022-04-06 17:22:24 +0100

    Draggable Bottom Sheet

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:draggable_bottom_sheet/draggable_bottom_sheet.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({Key? key}) : super(key: key);
@@ -10,15 +11,19 @@ class DetailScreen extends StatelessWidget {
         const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     final imageUrl = ModalRoute.of(context)!.settings.arguments as Map;
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-              imageUrl['image-link'],
+      body: DraggableBottomSheet(
+        backgroundWidget: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                imageUrl['image-link'],
+              ),
+              fit: BoxFit.cover,
             ),
-            fit: BoxFit.cover,
           ),
         ),
+        previewChild: Container(),
+        expandedChild: Container(),
       ),
     );
   }
